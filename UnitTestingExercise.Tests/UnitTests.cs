c85c90228557bff632bc75690c85c8bed0da6c79
Hannah Willis <hwillis8817@gmail.com>
2022-04-11 14:44:51 -0500

    inital commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -13,54 +13,64 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
+            var cal = new Calculator();
             
 
             //Act
                 // call the Add method that is located in the Calculator class
                 // and store its result in a variable named actual
+                var actual = cal.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+                Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(8,3,5)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
+            var cal = new Calculator();
 
             //Act
+           var actual = cal.Subtract(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(3,4,12)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
+            var cal = new Calculator();
 
             //Act
+            var actual = cal.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20,4,5)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
-            //Arrange
+            //Arrange 
+            var cal = new Calculator();
 
             //Act
+            var actual = cal.Divide(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
     }
