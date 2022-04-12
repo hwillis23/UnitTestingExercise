fd1e59145693e893f3735021542f11911b2aa6aa
Hannah Willis <hwillis8817@gmail.com>
2022-04-12 09:50:48 -0500

    added more numbers to test completed

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,6 +7,10 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(3, -6, -3, -6)]
+        [InlineData(14, 16, 30, 60)]
+        [InlineData(-16, -20, 6, -30)]
+
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -19,7 +23,8 @@ namespace UnitTestingExercise.Tests
             //Act
                 // call the Add method that is located in the Calculator class
                 // and store its result in a variable named actual
-                var actual = cal.Add(num1, num2, num3);
+                var actual = cal.Add(num1, num2, num3);  //whille store answer in actual
+
 
             //Assert
                 Assert.Equal(expected, actual);
@@ -27,6 +32,10 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(8,3,5)]//Add test data <-------
+        [InlineData(3, -6, 9)]
+        [InlineData(14, 16, -2)]
+        [InlineData(-16, 20, -36)]
+
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -35,7 +44,7 @@ namespace UnitTestingExercise.Tests
             var cal = new Calculator();
 
             //Act
-           var actual = cal.Subtract(minuend, subtrhend);
+           var actual =  cal.Subtract(minuend, subtrhend);
 
             //Assert
             Assert.Equal(expected, actual);
@@ -44,6 +53,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(3,4,12)]//Add test data <-------
+        [InlineData(3, -6, -18)]
+        [InlineData(14, 6, 84)]
+        [InlineData(-16, 2, -32)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -61,6 +73,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(20,4,5)]//Add test data <-------
+        [InlineData(-6, 3, -2)]
+        [InlineData(16, 4, 4)]
+        [InlineData(-20, 4, -5)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange 
